0e063cbf67e441bdb0968bce21a754e1f3210d46
sazzad <sazzadulislam7066@gmail.com>
2025-09-02 18:43:39 +0600

    grad

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-var num = 10;
+/*var num = 10;
 
 if (num %2 ==0)
     console.log("Even");
 if (num %2 !=0)
     console.log("Odd");
+*/
+// Student grading system
+var mark = prompt("Enter your mark:");
+
+if (mark>=80)
+    console.log("A+");
+else if (mark>=70)
+    console.log("A");
+else if (mark>=60)
+    console.log("B+");
+else if (mark>=50)
+    console.log("B-");
+else if (mark>=40)
+    console.log("C");
+
+else if(mark>=33)
+    console.log("D");
+
+else 
+    console.log("F");
+
