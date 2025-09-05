6ba5dd96f3d14ca5c30761e384511b55d0210f6d
sazzad <sazzadulislam7066@gmail.com>
2025-09-05 19:30:12 +0600

    while loop

--- index.js
+++ index.js
@@ -1,38 +1,9 @@
-//Digital spelling
-// 0 -zero, 1- one , 9.......Nine, 10-not a valid digit
-
-//var digit = prompt("Enter a any digit = : ");
-
-/* if (digit == 0)
-console.log(" Zero");
-
-else if (digit == 1)
-console.log(" One");
-else if (digit == 2)
-console.log(" Two");
-else if (digit == 3)
-console.log(" Three");
-else if (digit == 4)
-console.log(" Four");
-else if (digit == 5)
-console.log(" Five");
-else if (digit == 6)
-console.log(" Six");
-else if (digit == 7)
-console.log(" Seven");
-else if (digit == 8)
-console.log(" Eight");
-else if (digit == 9)
-console.log(" Nine");
-
-else
-    console.log(" not a valid digit");
-*/
-// for loop
-
+//2+4+6+8---------100
+var i = 2;
 var sum = 0;
-for(var x = 10; x <= 10; x = x + 1){
-    sum = sum + x;
-
+while (i <= 100){
+    sum = sum + i;
+    i = i + 2;
 }
-document.write(sum) ;
+document.write(sum);
+document.write("<h1>End</h1>");
