f3999d8779a19e5b0457cbd9d8ab50cea84e9214
sazzad <sazzadulislam7066@gmail.com>
2025-09-05 20:55:23 +0600

    do while loop

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-//2+4+6+8---------100
-var i = 2;
-var sum = 0;
-while (i <= 100){
-    sum = sum + i;
-    i = i + 2;
+// do while Loop
+// var i = 2;
+// do{
+// document.write(" " +i);
+// i = i +2;
+// }while(i <=100);
+
+//for loop 
+
+// for( var i = 1; i <=20; i = i + 2){
+//     document.write(" " +i);
+// }
+
+//while loop 
+
+var  i = 1;
+while( i <=20){
+    document.write(" " +i);
+    i = i + 3;
 }
-document.write(sum);
-document.write("<h1>End</h1>");
