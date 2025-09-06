8f6dadc9681f1d14248c3b1d22f2666f983572c6
sazzad <sazzadulislam7066@gmail.com>
2025-09-06 21:41:09 +0600

    function

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-// break keyword
-for (var i = 0; i <=100; i = i + 1){
-    if (i % 2 == 0){
-        continue;
-    }
-    document.write(" " + i);
+//function
+function addition(num1, num2){
+var result = num1 + num2;
+document.write("sum = " + result + "<br>");
 }
 
+
+//subtraction
+function subtraction(num1, num2){
+var result = num1 - num2;
+document.write("sub = " + result + "<br>");
+}
+//multiplication
+function multiplication(num1, num2){
+var result = num1 * num2;
+document.write("mul = " + result + "<br>");
+}
+//Division
+function division(num1, num2){
+var result = num1 / num2;
+document.write("div = " + result + "<br>");
+}
+
+addition(20,60);
+subtraction(20,60);
+multiplication(10,60);
+division(10,60);
