ccd0c9fe956818a29a1e266e3d99ce5155ff7061
sazzad <sazzadulislam7066@gmail.com>
2025-09-06 18:43:52 +0600

    break , continue

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-// do while Loop
-// var i = 2;
-// do{
-// document.write(" " +i);
-// i = i +2;
-// }while(i <=100);
-
-//for loop 
-
-// for( var i = 1; i <=20; i = i + 2){
-//     document.write(" " +i);
-// }
-
-//while loop 
-
-var  i = 1;
-while( i <=20){
-    document.write(" " +i);
-    i = i + 3;
+// break keyword
+for (var i = 0; i <=100; i = i + 1){
+    if (i % 2 == 0){
+        continue;
+    }
+    document.write(" " + i);
 }
+
