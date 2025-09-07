9d08971c4130d9ffc15bfdbc53332097a3f3e1db
sazzad <sazzadulislam7066@gmail.com>
2025-09-07 20:34:52 +0600

    array

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
-//function
-function addition(num1, num2){
-var result = num1 + num2;
-document.write("sum = " + result + "<br>");
-}
+//Array Methods
 
-
-//subtraction
-function subtraction(num1, num2){
-var result = num1 - num2;
-document.write("sub = " + result + "<br>");
-}
-//multiplication
-function multiplication(num1, num2){
-var result = num1 * num2;
-document.write("mul = " + result + "<br>");
-}
-//Division
-function division(num1, num2){
-var result = num1 / num2;
-document.write("div = " + result + "<br>");
-}
-
-addition(20,60);
-subtraction(20,60);
-multiplication(10,60);
-division(10,60);
+var names = ["sazzadu", "rakib", "sabbir"];
+console.log(names);
+names.shift();
+console.log(names);
