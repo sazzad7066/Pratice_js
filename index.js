e91894dd775e333918f1ed27fa4ae92023deda75
sazzad <sazzadulislam7066@gmail.com>
2025-09-08 21:04:44 +0600

    array

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 
 var names = ["sazzadu", "rakib", "sabbir"];
 console.log(names);
+// shift holo opposite of pop
 names.shift();
+console.log(names);
+//unshift holo opposite of push
+names.unshift("sazzad");
+console.log(names);
+//splice adding new element is an array
+names.splice(2, 0, " Rahim", " korim");
 console.log(names);
