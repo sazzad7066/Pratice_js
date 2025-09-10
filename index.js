ff2877d4f0643c60f6d36fd890603bffd11181f5
sazzad <sazzadulislam7066@gmail.com>
2025-09-10 15:43:18 +0600

    object

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-//Array Methods
-
-var names = ["sazzadu", "rakib", "sabbir"];
-console.log(names);
-// shift holo opposite of pop
-names.shift();
-console.log(names);
-//unshift holo opposite of push
-names.unshift("sazzad");
-console.log(names);
-//splice adding new element is an array
-names.splice(2, 0, " Rahim", " korim");
-console.log(names);
+//Object
+// student details
+var student = {
+    name: "sazzad",
+    age: 25,
+    cgpa: 3.5,
+    lang: ["Bangla", "English"]
+}
+console.log(student.name );
