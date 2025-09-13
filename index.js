0dea7d9db060d20d127d2a47657d3009ec01987d
sazzad <sazzadulislam7066@gmail.com>
2025-09-13 23:10:09 +0600

    Guess number

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-//Object
-// student details
-var student = {
-    name: "sazzad",
-    age: 25,
-    cgpa: 3.5,
-    lang: ["Bangla", "English"]
+//Guessing Game
+// Guess a number from 1  to 5
+// Generaste a random number between 1 to 5
+// If the Guess number matches random number; then show message won else lost
+// Run the game for 5 times
+//show the number of wons and losts
+
+var numOfWon = 0;
+var numOfLost = 0;
+for( i=1; i<=5; i++){
+
+     var guessNumber = parseInt(prompt("  Enter a number from 1 to 5 : "));
+
+var randomNumber = Math.floor( Math.random()*5) +1;
+
+if(guessNumber ==randomNumber){
+    console.log(" You have Won ");
+    numOfWon++;
+}else{
+    console.log(" You have lost.Random number was " + randomNumber);
+    numOfLost++;
+ }
+
+
 }
-console.log(student.name );
+
+document.write("Number of Won = " + numOfWon + "<br>");
+
+document.write("Number of Lost = " + numOfLost + "<br>");
