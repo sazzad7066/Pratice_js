3dfebf2f31cfce0ca97fedfd7fd4915afb942182
sazzad <sazzadulislam7066@gmail.com>
2025-09-28 15:46:23 +0600

    images dom

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-//javascript dom
-//querySelector()
-
-//document.querySelector("# name"). innerHTML = "sazzad";
-
-//button
-function myMessage(){
-    alert(" Hello Everyone ");
+var myVar = document.querySelector("#image");
+function Picture1() {
+    myVar.src = "images/sazzad1.jpg";
+    myVar.style.display = "block";
+}
+function Picture2() {
+    myVar.src = "images/sazzad2.jpg";
+    myVar.style.display = "block";
 }
